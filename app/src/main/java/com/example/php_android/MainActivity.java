1c227578f70d86d0428d3701561b13ffa8b2e422
409261263 <electricitycar@gmail.com>
2022-11-17 08:12:48 +0800

    first commit

---
+++ app/src/main/java/com/example/php_android/MainActivity.java
@@ -0,0 +1,62 @@
+package com.example.php_android;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MainActivity extends AppCompatActivity {
+    EditText e1;
+    Button b1;
+    String s;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        e1 = (EditText) findViewById(R.id.et1);
+        b1 = (Button) findViewById(R.id.save);
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                s = e1.getText().toString();
+                String url = "http://140.136.151.144/newlogin.php";
+                StringRequest stringRequest = new StringRequest(Request.Method.POST, url, new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        Toast.makeText(MainActivity.this, response.trim(), Toast.LENGTH_SHORT).show();
+                    }
+                },new Response.ErrorListener() {
+                            @Override
+                            public void onErrorResponse(VolleyError error) {
+                                Toast.makeText(MainActivity.this,error.toString(), Toast.LENGTH_SHORT).show();
+                            }
+
+
+                }){
+                    protected Map<String, String> getParams() {
+                        Map<String,String> params = new HashMap<String,String>();
+                        params.put("data",s);
+
+                        return params;
+                    }
+                };
+                RequestQueue requestQueue = Volley.newRequestQueue(MainActivity.this);
+                requestQueue.add(stringRequest);
+            }
+        });
+    }
+}
